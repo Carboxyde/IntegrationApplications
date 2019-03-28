b768ce4b891bfe4efc93cbc55d570edb82121174
BMosnier <bastienmosnier@gmail.com>
2019-03-28 21:27:29 +0100

    Add the Eye Tracking chronometer

---
+++ ChronometreReactif/ChronoBibliotheque/Chronometre.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+
+namespace ChronoBibliotheque
+{
+    public class Chronometre
+    {
+        int id = 0;
+
+        private const string URL = "http://localhost:8080/api/v1/chronometres/";
+
+        public Chronometre()
+        {
+            var request = (HttpWebRequest)WebRequest.Create(URL);
+
+            request.Method = "POST";
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var sr = new System.IO.StreamReader(stream))
+                    {
+                        id = int.Parse(sr.ReadToEnd());
+                    }
+                }
+            }
+        }
+
+        public int getTime()
+        {
+            var request = (HttpWebRequest)WebRequest.Create(URL+"/"+id);
+
+            request.Method = "GET";
+            var content = 0;
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var sr = new System.IO.StreamReader(stream))
+                    {
+                        content = int.Parse(sr.ReadToEnd());
+                    }
+                }
+            }
+
+            return content;
+        }
+    }
+}
---
+++ ChronometreReactif/ChronoWPF/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using ChronoBibliotheque;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tobii.Interaction.Wpf;
+
+namespace ChronoWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool clockHasGaze = false;
+        Task updateClock;
+        Chronometre chronometre=null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void updateTime(object sender, HasGazeChangedRoutedEventArgs e)
+        {
+                if (e.HasGaze)
+                {
+                    clockHasGaze = true;
+                    Action<object> action = (object obj) =>
+                    {
+                        while (clockHasGaze)
+                        {
+                            updateTimer();
+                            System.Threading.Thread.Sleep(100);
+
+                        }
+                    };
+                    updateClock = new Task(action, "update");
+                    updateClock.Start();
+                }
+                else
+                {
+                    clockHasGaze = false;
+                }
+        }
+
+        private void updateTimer()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (chronometre != null)
+                {
+                    var temps = chronometre.getTime();
+                    maTextBox.Text = "\n" + temps / 1000 + "." + temps % 1000;
+                }
+            });
+        }
+
+        private void startChrono(object sender, RoutedEventArgs e)
+        {
+            if (chronometre == null)
+            {
+
+                chronometre = new Chronometre();
+            }
+            else
+            {
+                updateTimer();
+                chronometre = null;
+            }
+        }
+    }
+}
